2df0345194e319a27e33eeafa489d5388581ff7b
YoncaEsendemir <yoncaesendemir.mdbf21@iste.edu.tr>
2025-03-30 03:12:44 +0300

    Mobile menu islevleri tamamlandi Home sayfasina giris yapildi

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,11 @@
+import { useEffect } from "react";
 import logo from "../logo/protfolio.png";
 
- const Navbar = ()=>{
-
+ const Navbar = ({menuOpen, setMenuOpen})=>{
+ 
+    useEffect( ()=>{
+           document.body.style.overflow=menuOpen ? "hidden" : "";
+    },[menuOpen]);
 
     return(
         <>
@@ -12,24 +16,26 @@ import logo from "../logo/protfolio.png";
                     <img src={logo} alt={"bulunamadi"} />
                 </a>
 
-               <div className="w-7 h-5 relative cursor-pointer z-40 md:hidden">
+               <div className="w-7 h-5 relative cursor-pointer z-40 md:hidden"
+                onClick={()=> setMenuOpen(!menuOpen)}>
+                   
                     &#9776;
                </div>
 
-               <div>
-                <a href="#home" className="tex-gray-300 hove:text-white transition-color mx-5 px-3">
+               <div className="hidden md:flex item-center space-x-8">
+                <a href="#home" className="text-gray-300 hove:text-white transition-color mx-5 px-3">
                     Home
                 </a>
 
-                <a href="#about" className="tex-gray-300 hove:text-white transition-color mx-5 px-3">
+                <a href="#about" className="text-gray-300 hove:text-white transition-color mx-5 px-3">
                     About
                 </a>
 
-                <a href="#projects" className="tex-gray-300 hove:text-white transition-color mx-5 px-3">
+                <a href="#projects" className="text-gray-300 hove:text-white transition-color mx-5 px-3">
                     Projects
                 </a>
 
-                <a href="#contact" className="tex-gray-300 hove:text-white transition-color mx-5 px-3">
+                <a href="#contact" className="text-gray-300 hove:text-white transition-color mx-5 px-3">
                     Contact
                 </a>
                </div>
---
+++ src/components/sections/Home.jsx
@@ -0,0 +1,15 @@
+
+const Homw = ()=>{
+    return(
+        <section
+        id="home" className="min-h-screen flex items-center justify-center relative">
+           <div className="text-center z-10 px-4">
+             <h1>
+                Hi, I'm Yonca Esendemir
+             </h1>
+           </div>
+        </section>
+    )
+}
+
+export default Homw
